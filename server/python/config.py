35dee13e700409eb4a2c80ece7694e367284a66b
JoymanCmr <cmoru520@hotmail.com>
2019-03-05 12:46:05 +0800

    修改配置文件

--- server/python/config.py
+++ server/python/config.py
@@ -1,11 +1,11 @@
 class DevelopConfig(object):
     DEBUG = True
     # redis 配置
-    REDIS_URI = 'redis://:my_redis@localhost:6379/0'
+    REDIS_URI = 'redis://:password@localhost:6379/0'
 
     # SQL 配置
     SQLALCHEMY_BINDS = {
-        'mysql_bind': 'mysql+pymysql://root:chenmoru@localhost/my_blog?charset=utf8mb4'
+        'mysql_bind': 'mysql+pymysql://username:password@localhost/my_blog?charset=utf8mb4'
     }
     pass
 
