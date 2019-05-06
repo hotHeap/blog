e768a506a1359425ae84f930889979dabefa02d2
Jancd <sergeychang@gmail.com>
2019-05-06 21:08:04 +0800

    common handler

--- server/golang/handler/handler.go
+++ server/golang/handler/handler.go
@@ -1,12 +1,35 @@
 package handler
 
-import "sunseekers/common/baseerror"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	"github.com/hotHeap/blog/server/golang/common/baseerror"
+)
+
+var (
+	errServiceUnavailable = errors.New("服务器竟然开小差，一会儿再试试吧")
+)
 
 type Handler struct {
-	Data string `json:"data"`
+	Data interface{} `json:"data"`
 	Err  *baseerror.CodeError
 }
 
-func NewHandler() {
+func NewHandler() *Handler {
+	return new(Handler)
+}
 
+func FormatResponse(context echo.Context, data interface{}, err error) error {
+	if err != nil {
+		codeErr, ok := baseerror.FromError(err)
+		if ok {
+			return context.JSON(http.StatusServiceUnavailable, codeErr)
+		} else {
+			return context.JSON(http.StatusInternalServerError, errServiceUnavailable)
+		}
+	}
+	return context.JSON(http.StatusOK, data)
 }
