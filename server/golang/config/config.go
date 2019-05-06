0b0f7205aa2a31cb39c81cac928bafaf0a220628
Jancd <sergeychang@gmail.com>
2019-05-06 21:06:37 +0800

    upgrade configuration

--- server/golang/config/config.go
+++ server/golang/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"strings"
+
 	"github.com/fsnotify/fsnotify"
 	"github.com/labstack/gommon/log"
 	"github.com/spf13/viper"
@@ -10,16 +12,35 @@ type Config struct {
 	Name string
 }
 
-func (cfg *Config) Init() error {
+func Init(cfg string) error {
+	config := Config{
+		Name: cfg,
+	}
+
+	// 用于热加载
+	//config.watchConfigChange()
+
+	return config.initConfig()
+}
+
+func (cfg *Config) initConfig() error {
+	if cfg.Name != "" {
+		viper.SetConfigFile(cfg.Name)
+	} else {
+		viper.AddConfigPath("conf") // 当前工程目录的 `conf` 文件夹
+		viper.SetConfigName("config")
+	}
+
+	viper.SetConfigType("yaml")     // 设置配置文件格式为YAML
+	viper.AutomaticEnv()            // 读取匹配的环境变量
+	viper.SetEnvPrefix("APISERVER") // 读取环境变量的前缀为APISERVER
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	if err := viper.ReadInConfig(); err != nil { // viper解析配置文件
+		return err
+	}
+
 	return nil
-}
-
-func (cfg *Config) initConfig() {
-
-}
-
-func (cfg *Config) intLog() {
-
 }
 
 func (cfg *Config) watchConfigChange() {
@@ -27,4 +48,9 @@ func (cfg *Config) watchConfigChange() {
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		log.Infof("Config file changed: %s", in.Name)
 	})
+}
+
+// TODO
+func (cfg *Config) intLog() {
+
 }
