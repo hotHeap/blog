373040421a8697da9247d5e2b1d7e7a5e5867107
Jancd <sergeychang@gmail.com>
2019-05-06 21:10:06 +0800

    Update main.go

--- server/golang/main.go
+++ server/golang/main.go
@@ -1,37 +1,72 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/spf13/pflag"
-
-	"github.com/labstack/echo"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/middleware"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"
+
+	"github.com/hotHeap/blog/server/golang/config"
+	"github.com/hotHeap/blog/server/golang/router"
 )
 
 var (
-	cfg     = pflag.StringP("config", "c", "", "blog api server configuration path")
-	version = pflag.BoolP("version", "v", false, "blog api server's version")
+	version       = pflag.BoolP("version", "v", false, "blog api server's version")
+	configuration = pflag.StringP("config", "c", "", "blog api server configuration path")
 )
 
 func main() {
 	pflag.Parse()
 
+	// init configuration
+	if err := config.Init(*configuration); err != nil {
+		log.Fatal("config error:", err)
+	}
+
+	db, err := initDB()
+	if err != nil {
+		log.Fatal("database error:", err)
+	}
+	defer db.Close()
+
 	// Echo instance
 	e := echo.New()
+	e.GET("/ping", pong)
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// middleware
+	useMiddleware(e)
 
 	// Routes
-	e.GET("/", hello)
+	router.LoadRouter(e, db)
 
 	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func pong(c echo.Context) error {
+	return c.String(http.StatusOK, "pong!")
+}
+
+func initDB() (*sqlx.DB, error) {
+	config := fmt.Sprintf("%s:%s@tcp(%s)/%s?",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.address"),
+		viper.GetString("db.name"),
+	)
+	return sqlx.Connect(viper.GetString("db.drivename"), config+"parseTime=true&loc=Asia%2FShanghai")
+}
+
+func useMiddleware(e *echo.Echo) {
+	e.Use(middleware.CORS())
+	e.Use(middleware.Secure())
+	e.Use(middleware.Gzip())
 }
